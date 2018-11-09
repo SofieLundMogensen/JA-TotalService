65bb16281b5cebba4e47a5297edde5adfe0a0ef2
Sofie Lund Mogensen <Sofie@lundmogensen.dk>
2018-11-09 09:58:29 +0100

    initial comit

---
+++ JATotalservice/JATotalservice/JATotalservice.Core/App.cs
@@ -0,0 +1,17 @@
+using MvvmCross.Platform.IoC;
+
+namespace JATotalservice.Core
+{
+    public class App : MvvmCross.Core.ViewModels.MvxApplication
+    {
+        public override void Initialize()
+        {
+            CreatableTypes()
+                .EndingWith("Service")
+                .AsInterfaces()
+                .RegisterAsLazySingleton();
+
+            RegisterNavigationServiceAppStart<ViewModels.FirstViewModel>();
+        }
+    }
+}
---
+++ JATotalservice/JATotalservice/JATotalservice.WPF/Views/FirstView.xaml.cs
@@ -0,0 +1,12 @@
+using MvvmCross.Wpf.Views;
+
+namespace JATotalservice.WPF.Views
+{
+    public partial class FirstView : MvxWpfView
+    {
+        public FirstView()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
