81e19231377c20c530240b992ddc6cd8dce8eff6
Dennis Sørensen <dennissorensen24@hotmail.dk>
2018-11-15 14:15:49 +0100

    Created binding from TimeViewModel to TimeView

--- JATotalservice/JATotalservice/JATotalservice.Core/ViewModels/TimeViewModel.cs
+++ JATotalservice/JATotalservice/JATotalservice.Core/ViewModels/TimeViewModel.cs
@@ -1,10 +1,18 @@
-﻿using System;
+﻿using MvvmCross.Core.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace JATotalservice.Core.ViewModels
 {
-    class TimeViewModel
+    public class TimeViewModel : MvxViewModel
     {
+        string testString = "Du er i TimeView'et";
+
+        public string TestString
+        {
+            get { return testString; }
+            set { SetProperty(ref testString, value); }
+        }
     }
 }
--- JATotalservice/JATotalservice/JATotalservice.Droid/Views/TimeView.cs
+++ JATotalservice/JATotalservice/JATotalservice.Droid/Views/TimeView.cs
@@ -9,17 +9,35 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using JATotalservice.Core.ViewModels;
+using MvvmCross.Binding.BindingContext;
 
 namespace JATotalservice.Droid.Views
 {
     [Activity(Label = "View for TimeViewModel")]
     public class TimeView : BaseView
     {
+        
         protected override int LayoutResource => Resource.Layout.TimeView;
+
+        TextView testTextView;
 
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
+
+            testTextView = FindViewById<TextView>(Resource.Id.testTextView);
+
+            SetupBindings();
+        }
+
+        protected void SetupBindings()
+        {
+            var set = this.CreateBindingSet<TimeView, TimeViewModel>();
+
+            set.Bind(testTextView).For(m => m.Text).To(vm => vm.TestString);
+
+            set.Apply();
         }
     }
 }
