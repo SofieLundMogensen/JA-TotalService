3951b1b21915c91604f46dd64024b5f04f84b589
Dennis Sørensen <dennissorensen24@hotmail.dk>
2018-11-16 11:48:50 +0100

    Added temporary modellayer to core

---
+++ JATotalservice/JATotalservice/JATotalservice.Core/ModelLayer/Material.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelLayer
+{
+    public class Material
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public float price { get; set; }
+        public string description { get; set; }
+    }
+}
---
+++ JATotalservice/JATotalservice/JATotalservice.Core/ModelLayer/TimeRegistartion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelLayer
+{
+    public class TimeRegistartion
+    {
+        public DateTime startTime { get; set; }
+        public DateTime endTime { get; set; }
+    }
+}
