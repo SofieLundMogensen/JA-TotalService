cb02f1b5c4605f4990cb788a98a2878ea53cea4f
Sofie Lund Mogensen <Sofie@lundmogensen.dk>
2018-11-20 11:54:03 +0100

    Timeregistartion Test done

--- JATotalserviceAPI/DataAccessLayer/DBTimeRegistration.cs
+++ JATotalserviceAPI/DataAccessLayer/DBTimeRegistration.cs
@@ -19,8 +19,8 @@ namespace BusinessLogicLayer
 
                 MySqlCommand cmd = new MySqlCommand();
                 cmd.Connection = conn;
-                cmd.CommandText = "INSERT INTO `TimeRegistration`( `EndTime`, `TaskId`, `StartTime`, `EmployeeId`) VALUES(?,?,?,?)";
-               // cmd.Parameters.Add("?Id", MySqlDbType.Int32).Value = obj.Id;
+                cmd.CommandText = "INSERT INTO `TimeRegistration`(`Id`, `EndTime`, `TaskId`, `StartTime`, `EmployeeId`) VALUES(?,?,?,?,?)";
+                cmd.Parameters.Add("?Id", MySqlDbType.Int32).Value = obj.Id;
                 cmd.Parameters.Add("?EndTime", MySqlDbType.DateTime).Value = obj.endTime;
                 cmd.Parameters.Add("?TaskId", MySqlDbType.Int32).Value = obj.task.id;
                 cmd.Parameters.Add("?StartTime", MySqlDbType.DateTime).Value = obj.startTime;
