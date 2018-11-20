2d59b049d06a566f1e1f1b67f7bef79f858d3377
Sofie Lund Mogensen <Sofie@lundmogensen.dk>
2018-11-20 15:49:20 +0100

    Timeview

--- JATotalservice/JATotalservice/JATotalservice.Droid/Resources/Resource.Designer.cs
+++ JATotalservice/JATotalservice/JATotalservice.Droid/Resources/Resource.Designer.cs
@@ -6030,6 +6030,9 @@ namespace JATotalservice.Droid
 			// aapt resource value: 0x7f090010
 			public const int MvxBindingTagUnique = 2131296272;
 			
+			// aapt resource value: 0x7f0900a6
+			public const int TextLastname = 2131296422;
+			
 			// aapt resource value: 0x7f090091
 			public const int action0 = 2131296401;
 			
@@ -6186,6 +6189,9 @@ namespace JATotalservice.Droid
 			// aapt resource value: 0x7f090014
 			public const int disableHome = 2131296276;
 			
+			// aapt resource value: 0x7f0900a5
+			public const int dropdown = 2131296421;
+			
 			// aapt resource value: 0x7f090068
 			public const int edit_query = 2131296360;
 			
@@ -6270,8 +6276,8 @@ namespace JATotalservice.Droid
 			// aapt resource value: 0x7f09004a
 			public const int list_item = 2131296330;
 			
-			// aapt resource value: 0x7f0900a7
-			public const int masked = 2131296423;
+			// aapt resource value: 0x7f0900ab
+			public const int masked = 2131296427;
 			
 			// aapt resource value: 0x7f090094
 			public const int media_actions = 2131296404;
@@ -6393,6 +6399,9 @@ namespace JATotalservice.Droid
 			// aapt resource value: 0x7f090073
 			public const int search_voice_btn = 2131296371;
 			
+			// aapt resource value: 0x7f0900a7
+			public const int select_button = 2131296423;
+			
 			// aapt resource value: 0x7f090074
 			public const int select_dialog_listview = 2131296372;
 			
@@ -6450,9 +6459,6 @@ namespace JATotalservice.Droid
 			// aapt resource value: 0x7f090013
 			public const int tabMode = 2131296275;
 			
-			// aapt resource value: 0x7f0900a5
-			public const int testTextView = 2131296421;
-			
 			// aapt resource value: 0x7f0900a3
 			public const int text = 2131296419;
 			
@@ -6479,6 +6485,12 @@ namespace JATotalservice.Droid
 			
 			// aapt resource value: 0x7f090099
 			public const int time = 2131296409;
+			
+			// aapt resource value: 0x7f0900a9
+			public const int timePicker1 = 2131296425;
+			
+			// aapt resource value: 0x7f0900a8
+			public const int time_display = 2131296424;
 			
 			// aapt resource value: 0x7f09004c
 			public const int title = 2131296332;
@@ -6519,8 +6531,8 @@ namespace JATotalservice.Droid
 			// aapt resource value: 0x7f09000e
 			public const int view_offset_helper = 2131296270;
 			
-			// aapt resource value: 0x7f0900a6
-			public const int visible = 2131296422;
+			// aapt resource value: 0x7f0900aa
+			public const int visible = 2131296426;
 			
 			// aapt resource value: 0x7f090029
 			public const int withText = 2131296297;
@@ -6666,121 +6678,130 @@ namespace JATotalservice.Droid
 			public const int abc_select_dialog_material = 2130968602;
 			
 			// aapt resource value: 0x7f04001b
-			public const int design_bottom_navigation_item = 2130968603;
+			public const int Clock = 2130968603;
 			
 			// aapt resource value: 0x7f04001c
-			public const int design_bottom_sheet_dialog = 2130968604;
+			public const int design_bottom_navigation_item = 2130968604;
 			
 			// aapt resource value: 0x7f04001d
-			public const int design_layout_snackbar = 2130968605;
+			public const int design_bottom_sheet_dialog = 2130968605;
 			
 			// aapt resource value: 0x7f04001e
-			public const int design_layout_snackbar_include = 2130968606;
+			public const int design_layout_snackbar = 2130968606;
 			
 			// aapt resource value: 0x7f04001f
-			public const int design_layout_tab_icon = 2130968607;
+			public const int design_layout_snackbar_include = 2130968607;
 			
 			// aapt resource value: 0x7f040020
-			public const int design_layout_tab_text = 2130968608;
+			public const int design_layout_tab_icon = 2130968608;
 			
 			// aapt resource value: 0x7f040021
-			public const int design_menu_item_action_area = 2130968609;
+			public const int design_layout_tab_text = 2130968609;
 			
 			// aapt resource value: 0x7f040022
-			public const int design_navigation_item = 2130968610;
+			public const int design_menu_item_action_area = 2130968610;
 			
 			// aapt resource value: 0x7f040023
-			public const int design_navigation_item_header = 2130968611;
+			public const int design_navigation_item = 2130968611;
 			
 			// aapt resource value: 0x7f040024
-			public const int design_navigation_item_separator = 2130968612;
+			public const int design_navigation_item_header = 2130968612;
 			
 			// aapt resource value: 0x7f040025
-			public const int design_navigation_item_subheader = 2130968613;
+			public const int design_navigation_item_separator = 2130968613;
 			
 			// aapt resource value: 0x7f040026
-			public const int design_navigation_menu = 2130968614;
+			public const int design_navigation_item_subheader = 2130968614;
 			
 			// aapt resource value: 0x7f040027
-			public const int design_navigation_menu_item = 2130968615;
+			public const int design_navigation_menu = 2130968615;
 			
 			// aapt resource value: 0x7f040028
-			public const int design_text_input_password_icon = 2130968616;
+			public const int design_navigation_menu_item = 2130968616;
 			
 			// aapt resource value: 0x7f040029
-			public const int EstimateView = 2130968617;
+			public const int design_text_input_password_icon = 2130968617;
 			
 			// aapt resource value: 0x7f04002a
-			public const int FirstView = 2130968618;
+			public const int EstimateView = 2130968618;
 			
 			// aapt resource value: 0x7f04002b
-			public const int MaterialsView = 2130968619;
+			public const int FirstView = 2130968619;
 			
 			// aapt resource value: 0x7f04002c
-			public const int notification_action = 2130968620;
+			public const int MaterialsView = 2130968620;
 			
 			// aapt resource value: 0x7f04002d
-			public const int notification_action_tombstone = 2130968621;
+			public const int notification_action = 2130968621;
 			
 			// aapt resource value: 0x7f04002e
-			public const int notification_media_action = 2130968622;
+			public const int notification_action_tombstone = 2130968622;
 			
 			// aapt resource value: 0x7f04002f
-			public const int notification_media_cancel_action = 2130968623;
+			public const int notification_media_action = 2130968623;
 			
 			// aapt resource value: 0x7f040030
-			public const int notification_template_big_media = 2130968624;
+			public const int notification_media_cancel_action = 2130968624;
 			
 			// aapt resource value: 0x7f040031
-			public const int notification_template_big_media_custom = 2130968625;
+			public const int notification_template_big_media = 2130968625;
 			
 			// aapt resource value: 0x7f040032
-			public const int notification_template_big_media_narrow = 2130968626;
+			public const int notification_template_big_media_custom = 2130968626;
 			
 			// aapt resource value: 0x7f040033
-			public const int notification_template_big_media_narrow_custom = 2130968627;
+			public const int notification_template_big_media_narrow = 2130968627;
 			
 			// aapt resource value: 0x7f040034
-			public const int notification_template_custom_big = 2130968628;
+			public const int notification_template_big_media_narrow_custom = 2130968628;
 			
 			// aapt resource value: 0x7f040035
-			public const int notification_template_icon_group = 2130968629;
+			public const int notification_template_custom_big = 2130968629;
 			
 			// aapt resource value: 0x7f040036
-			public const int notification_template_lines_media = 2130968630;
+			public const int notification_template_icon_group = 2130968630;
 			
 			// aapt resource value: 0x7f040037
-			public const int notification_template_media = 2130968631;
+			public const int notification_template_lines_media = 2130968631;
 			
 			// aapt resource value: 0x7f040038
-			public const int notification_template_media_custom = 2130968632;
+			public const int notification_template_media = 2130968632;
 			
 			// aapt resource value: 0x7f040039
-			public const int notification_template_part_chronometer = 2130968633;
+			public const int notification_template_media_custom = 2130968633;
 			
 			// aapt resource value: 0x7f04003a
-			public const int notification_template_part_time = 2130968634;
+			public const int notification_template_part_chronometer = 2130968634;
 			
 			// aapt resource value: 0x7f04003b
-			public const int select_dialog_item_material = 2130968635;
+			public const int notification_template_part_time = 2130968635;
 			
 			// aapt resource value: 0x7f04003c
-			public const int select_dialog_multichoice_material = 2130968636;
+			public const int select_dialog_item_material = 2130968636;
 			
 			// aapt resource value: 0x7f04003d
-			public const int select_dialog_singlechoice_material = 2130968637;
+			public const int select_dialog_multichoice_material = 2130968637;
 			
 			// aapt resource value: 0x7f04003e
-			public const int SplashScreen = 2130968638;
+			public const int select_dialog_singlechoice_material = 2130968638;
 			
 			// aapt resource value: 0x7f04003f
-			public const int support_simple_spinner_dropdown_item = 2130968639;
+			public const int SpinnerItemDropdown = 2130968639;
 			
 			// aapt resource value: 0x7f040040
-			public const int TimeView = 2130968640;
+			public const int SpinnerItemText = 2130968640;
 			
 			// aapt resource value: 0x7f040041
-			public const int toolbar = 2130968641;
+			public const int SplashScreen = 2130968641;
+			
+			// aapt resource value: 0x7f040042
+			public const int support_simple_spinner_dropdown_item = 2130968642;
+			
+			// aapt resource value: 0x7f040043
+			public const int TimeView = 2130968643;
+			
+			// aapt resource value: 0x7f040044
+			public const int toolbar = 2130968644;
 			
 			static Layout()
 			{
--- JATotalservice/JATotalservice/JATotalservice.Droid/Views/TimeView.cs
+++ JATotalservice/JATotalservice/JATotalservice.Droid/Views/TimeView.cs
@@ -10,6 +10,8 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using JATotalservice.Core.ViewModels;
+using JATotalservice.Droid.Adapter;
+using ModelLayer;
 using MvvmCross.Binding.BindingContext;
 
 namespace JATotalservice.Droid.Views
@@ -17,18 +19,42 @@ namespace JATotalservice.Droid.Views
     [Activity(Label = "View for TimeViewModel")]
     public class TimeView : BaseView
     {
-        
+
         protected override int LayoutResource => Resource.Layout.TimeView;
 
         TextView testTextView;
-
+        TextView timeDisplay;
+        Button timeSelectButton;
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
-            testTextView = FindViewById<TextView>(Resource.Id.testTextView);
+            //testTextView = FindViewById<TextView>(Resource.Id.testTextView);
+            var som = FindViewById<Spinner>(Resource.Id.dropdown);
+            Task task = new Task();
+            var test = new List<Task>();
+            test.Add(new Task { name = "Hans HANSEN VEJ"});
+            test.Add(new Task { name = "Ib" });
+            test.Add(new Task { name = "Bo" });
+            som.Adapter = new DropDownTaskAdapter(test, this);
+
+            timeDisplay = FindViewById<TextView>(Resource.Id.time_display);
+           timeSelectButton = FindViewById<Button>(Resource.Id.select_button);
+
+            timeSelectButton.Click += TimeSelectOnClick;
 
             SetupBindings();
+        }
+        void TimeSelectOnClick(object sender, EventArgs eventArgs)
+        {
+            TimePickerFragment frag = TimePickerFragment.NewInstance(
+                delegate (DateTime time)
+                {
+                    timeDisplay.Text = time.ToShortTimeString();
+                });
+        
+            frag.Show(FragmentManager, TimePickerFragment.TAG);
+           
         }
 
         protected void SetupBindings()
