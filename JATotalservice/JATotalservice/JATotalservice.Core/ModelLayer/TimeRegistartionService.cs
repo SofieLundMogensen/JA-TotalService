b456e285443d9b4c8a7dcf7e69a3ec8c90176562
Sofie Lund Mogensen <Sofie@lundmogensen.dk>
2018-11-21 11:47:46 +0100

    Post

--- JATotalservice/JATotalservice/JATotalservice.Core/ModelLayer/TimeRegistartionService.cs
+++ JATotalservice/JATotalservice/JATotalservice.Core/ModelLayer/TimeRegistartionService.cs
@@ -1,14 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using ModelLayer;
 using RestSharp;
-
 
 namespace JATotalservice.Core.ModelLayer
 {
     public class TimeRegistartionService
     {
+        private static readonly HttpClient _Client = new HttpClient();
+
+
+
         public static TimeRegistartion getTimeInfo(int id)
         {
             RestClient client = new RestClient("http://jatotalservice.slund.info/api/TimeRegistration/Get{id}");
@@ -19,24 +25,50 @@ namespace JATotalservice.Core.ModelLayer
             return timeRegistartion;
         }
 
-        public static void postTimeInfo(TimeRegistartion timeregistration)
+        public static async System.Threading.Tasks.Task postTimeInfoAsync(TimeRegistartion timeregistration)
         {
-            RestClient client = new RestClient("http://jatotalservice.slund.info/api/TimeRegistration/Post");
-            var reguest = new RestRequest();
-            reguest.Method = Method.POST;
-            reguest.AddHeader("Accept", "application/json");
-            reguest.Parameters.Clear();
-            reguest.AddObject(timeregistration);
-            var response = client.Execute(reguest);
-           /* IRestRequest request = new RestRequest(Method.POST);
-            request.AddHeader("Accept", "application/Json");
-            request.AddObject(timeregistration);
-            request.RequestFormat = DataFormat.Json;
-            request.JsonSerializer.ContentType = "text/Json";
-            var response = client.Execute(request);*/
-            
-            
+            /* RestClient client = new RestClient("http://jatotalservice.slund.info/api/TimeRegistration/Post");
+             var reguest = new RestRequest();
+             reguest.Method = Method.POST;
+             reguest.AddHeader("Accept", "application/json");
+             reguest.Parameters.Clear();
+             reguest.AddObject(timeregistration);
+             var response = client.Execute(reguest);
+            /* IRestRequest request = new RestRequest(Method.POST);
+             request.AddHeader("Accept", "application/Json");
+             request.AddObject(timeregistration);
+             request.RequestFormat = DataFormat.Json;
+             request.JsonSerializer.ContentType = "text/Json";
+             var response = client.Execute(request);*/
+
+            var json = Newtonsoft.Json.JsonConvert.SerializeObject(timeregistration);
+
+
+            string url = "http://jatotalservice.slund.info/api/TimeRegistration/Post";
+
+
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Method = "POST";
+
+            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            {
+
+
+                streamWriter.Write(json);
+                streamWriter.Flush();
+                streamWriter.Close();
+            }
+
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                var result = streamReader.ReadToEnd();
+            }
+
+
         }
     }
-
 }
+
+
