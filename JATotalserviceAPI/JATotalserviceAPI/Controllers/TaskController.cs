884a61a4e8ab9e988a14d462a46f2c8d07fb429a
Marc Jensen <marc.r.jensen@gmail.com>
2018-12-04 09:10:49 +0100

    ServiceAPI TaskController

---
+++ JATotalserviceAPI/JATotalserviceAPI/Controllers/TaskController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ModelLayer;
+
+namespace ServiceAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+        BusinessLogicLayer.TaskController taskcontroller = new BusinessLogicLayer.TaskController();
+
+        // Get api/Tasks
+        [HttpGet("GetAll")]
+         public List<Task> GetAll()
+        {
+            return taskcontroller.GetAll();
+        }
+
+        //GET api/Task
+        [HttpGet(("get") + "{id}")]
+        public Task Get(int id)
+        {
+            return taskcontroller.Get(id);
+        }
+
+        // POST api/Task
+        [HttpPost("Post")]
+            public bool Post(Task task)
+        {
+            return taskcontroller.Create(task);
+        }
+
+        //Put api/Task
+        [HttpPost("Put")]
+        public bool Put(Task task)
+        {
+            return taskcontroller.Update(task);
+        }
+
+        // Delete api/Task
+        [HttpDelete(("Delete")+("{id}"))]
+        public bool Delete(int id)
+        {
+        return taskcontroller.Delete(id);
+        }
+    }
+}
