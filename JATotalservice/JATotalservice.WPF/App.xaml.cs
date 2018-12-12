462a35341b7eac736836df3c3363f3193163d8b8
Sofie Lund Mogensen <Sofie@lundmogensen.dk>
2018-12-12 11:18:38 +0100

    WPF

---
+++ JATotalservice/JATotalservice.WPF/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using MvvmCross.Core;
+using MvvmCross.Platforms.Wpf.Core;
+using MvvmCross.Platforms.Wpf.Views;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace JATotalservice.WPF
+{
+    public partial class App : MvxApplication
+    {
+        protected override void RegisterSetup()
+        {
+            this.RegisterSetupType<MvxWpfSetup<Core.App>>();
+        }
+
+
+    }
+}
---
+++ JATotalservice/JATotalservice.WPF/Views/FirstView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using JATotalservice.Core.ViewModels;
+using MvvmCross.Platforms.Wpf.Views;
+using MvvmCross.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JATotalservice.WPF.Views
+{
+    /// <summary>
+    /// Interaction logic for FirstView.xaml
+    /// </summary>
+    [MvxViewFor(typeof(FirstViewModel))]
+    public partial class FirstView : MvxWpfView
+    {
+        public FirstView()
+        {
+            InitializeComponent();
+        }
+    }
+}
