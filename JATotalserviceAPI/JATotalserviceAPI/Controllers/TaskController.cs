c162088a8b9ce1f7aabbc37945f7aa0f89779ad1
Dennis Sørensen <dennissorensen24@hotmail.dk>
2018-12-17 10:06:03 +0100

    tilføjet timeRegistrations på Task, når man laver en GetAll

--- JATotalserviceAPI/JATotalserviceAPI/Controllers/TaskController.cs
+++ JATotalserviceAPI/JATotalserviceAPI/Controllers/TaskController.cs
@@ -12,12 +12,41 @@ namespace ServiceAPI.Controllers
     public class TaskController : ControllerBase
     {
         BusinessLogicLayer.TaskController taskcontroller = new BusinessLogicLayer.TaskController();
+        BusinessLogicLayer.TimeRegistrationController timeRegistrationController = new BusinessLogicLayer.TimeRegistrationController();
+        BusinessLogicLayer.MaterialController materialController = new BusinessLogicLayer.MaterialController();
 
         // Get api/Tasks
         [HttpGet("GetAll")]
         public List<Task> GetAll()
         {
-            return taskcontroller.GetAll();
+            /* En alternativ til koden, er at lave en left join i da kaldet, sql koden er nedenfor, hvor man left joiner task sammen med timeRegistrations
+                SELECT `Task`.`Id`, `Task`.`Name`, `Task`.`Description`, `Task`.`IsComplete`, 
+                    `TimeRegistration`.`Id`, `TimeRegistration`.`TaskId`, `TimeRegistration`.`StartTime`, `TimeRegistration`.`EndTime`, `TimeRegistration`.`EmployeeId`
+                FROM
+                    `Task`
+                LEFT JOIN
+                    `TimeRegistration` ON `Task`.`Id` = `TimeRegistration`.`TaskId`
+                ORDER BY `Task`.`Id`
+            */
+
+            var taskList = taskcontroller.GetAll();
+            var timeList = timeRegistrationController.GetAll();
+            var materialList = materialController.GetAll();
+            //Tilføjer task og timeRegistrations sammen
+            for (int i = 0; i < taskList.Count; i++) //Man kan ikke bruge foreach ved denne løsning
+            {
+                taskList[i].timeRegistrations = new List<TimeRegistartion>(); //Instansiere listen timeRegistrations på listen af task
+                foreach (var time in timeList)
+                {
+                    if (taskList[i].id == time.task.id) //Spøreger om id'erne er det samme, og tilføjer dem hvis de er
+                    {
+                        taskList[i].timeRegistrations.Add(time);
+                    }
+                }
+            }
+
+  
+            return taskList;
         }
 
         //GET api/Task
